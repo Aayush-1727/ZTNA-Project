fbfb2a3dd0f15df1c6d58e7b359c53696ae45b37
SHIRISH SHETTY <supremetuluvaa77@gmail.com>
2026-06-22 20:44:29 +0530

    git commit

--- models/User.js
+++ models/User.js
@@ -1,12 +1,34 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  currentToken: {
-  type: String
-}
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    currentToken: {
+      type: String,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
